21b4663e713426131eaa1acd74ec356948a25db5
nickschafran <nickschafran@gmail.com>
2017-07-09 23:08:55 -0400

    More aliases

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,9 +1,13 @@
 set work ~/Documents/03_work
 
-alias gs      'git status -sb'
-alias ga      'git add'
-alias gc      'git commit -m'
-alias gp      'git pull'
-alias gps     'git push'
-alias gl      'git log --color --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset" --abbrev-commit'
-alias gst     'git stash'
+alias gs 'git status -sb'
+alias ga 'git add'
+alias gc 'git commit -m'
+alias gp 'git pull'
+alias gps 'git push'
+alias gl 'git log --color --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset" --abbrev-commit'
+alias gst 'git stash'
+
+alias firefox 'open -a firefox'
+alias chrome 'open -a "Google Chrome"'
+alias safari 'open -a safari'
